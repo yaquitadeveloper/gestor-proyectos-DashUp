cb24079d49a2108cee92475ffcb1526ecb561e4f
yaquitadeveloper <yaquitadeveloper@gmail.com>
2025-08-29 16:32:24 -0500

    Configurando el SSR de Astro y el adaptador de Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon()]
+  integrations: [icon()],
+  adapter: vercel()
 });
