ed16c363c996ef8647b9c403767f83929bbc4174
yaquitadeveloper <yaquitadeveloper@gmail.com>
2025-08-29 17:15:47 -0500

    corrigiendo astro.config.msj

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   integrations: [icon()],
-  adapter: vercel()
+  output: "server",
+  adapter: vercel(),
 });
