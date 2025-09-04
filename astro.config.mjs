adb10a78b60cacd3472e296b0fd0faa3c529b845
yaquitadeveloper <yaquitadeveloper@gmail.com>
2025-09-04 15:44:28 -0500

    reemplazar los svg por iconos de astro y refactorizando de los componentes del landing page y  configurar anstro.config.msj

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,13 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
-import icon from "astro-icon";
-
+import astroIcon from "astro-icon";
 import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [icon()],
+  integrations: [astroIcon()],
   output: "server",
   adapter: vercel(),
 });
